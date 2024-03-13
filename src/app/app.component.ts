433b9323665e8deea05ff13ade07f2d97c849746
Hemakshi <hemakshishukla@gmail.com>
2024-03-13 20:44:48 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { AboutComponent } from './Components/about/about.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ContactComponent } from './Components/contact/contact.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, HomeComponent, ContactComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'work';
+}
