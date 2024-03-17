9135e7526aa5c4dd846d8614a80389ee4f1f4797
Hemakshi <hemakshishukla@gmail.com>
2024-03-17 11:43:49 +0530

    added footer and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { AboutComponent } from './Components/about/about.component';
 import { HomeComponent } from './Components/home/home.component';
@@ -8,7 +8,7 @@ import { ContactComponent } from './Components/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboutComponent, HomeComponent, ContactComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, HomeComponent, ContactComponent, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'#', component:HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
+];
